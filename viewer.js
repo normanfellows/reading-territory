9000f41036691fa9645b58273db596155be9b1d2
Norman Fellows <norman.fellows@orange.fr>
2026-08-11 08:35:58 +0200

    Add files via upload
    Previously missing

---
+++ viewer.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Reading Territory — A Proposition in Formation");
+
+    Mirador.viewer({
+        id: "mirador-viewer",
+        windows: [
+            {
+                manifestId: "manifest.json"
+            }
+        ]
+    });
+
+});
